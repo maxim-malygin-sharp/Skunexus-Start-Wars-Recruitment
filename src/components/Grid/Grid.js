a26cc2ce04bfdb2ba800c9b5c943fced0419cfe1
Maxim Malygin <maxim.malygin@sharp-dev.net>
2020-11-12 15:33:51 +0400

    design finish

--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -6,24 +6,26 @@ function Grid({data: {header = [], values = [], actions = []}}) {
 
 
   return (
-    <table className='gridTable'>
-      <thead>
-        <tr>
-          {header.map(colName => <th key={colName}>{colName}</th>)}
-          {!!actions.length && <th>Actions</th>}
-        </tr>
-      </thead>
-      <tbody>
-        {values.map((row, index) => (
-          <GridItem
-            key={index}
-            row={row} 
-            header={header}
-            actions={actions}
-          />
-        ))}
-      </tbody>
-    </table>
+    <div className="container">
+      <table className='gridTable'>
+        <thead>
+          <tr>
+            {header.map(colName => <th key={colName}>{colName}</th>)}
+            {!!actions.length && <th>Actions</th>}
+          </tr>
+        </thead>
+        <tbody>
+          {values.map((row, index) => (
+            <GridItem
+              key={index}
+              row={row}
+              header={header}
+              actions={actions}
+            />
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
