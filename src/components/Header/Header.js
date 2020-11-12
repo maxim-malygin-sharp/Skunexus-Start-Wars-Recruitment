597884e9b2c1ff2339ba627664973d76de4fbf8f
Maxim Malygin <maxim.malygin@sharp-dev.net>
2020-11-12 14:54:43 +0400

    design theme

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,6 +5,10 @@ const Header = (props) => {
   
   return ( 
     <header className="header">
+      <a href="/" className="header__logo logo">
+        <img src="/images/logo.svg" alt="Star Wars" widht="90" height="40" className="logo__image" />
+        <span className="logo__text">Star Wars Planet Database</span>
+      </a>
       {children}
     </header>
   );
